e081fa003605694707e2a402597892158581a74a
jeniscodes <61928262+jeniscodes@users.noreply.github.com>
2021-05-07 15:23:50 +0545

    Update settings.py

--- capstone/settings.py
+++ capstone/settings.py
@@ -10,8 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 import django_heroku
-
-
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
